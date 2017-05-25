fb9754727e845b997953802f2b4ab72f12f2a2f7
Yogesh Khatri <yogesh.khatri@rediffmail.com>
2017-05-25 09:32:39 +0530

    this is my first commit_helloWorld Program of Eclipse

---
+++ helpac/Variable.java
@@ -0,0 +1,16 @@
+package helpac;
+
+public class Variable {
+
+	public static void main(String[] args) {
+	int x;
+	int y, z;
+	
+	x=10;
+	y=3;
+	z=x % y;
+	System.out.println(" Total Modulus " + z);
+
+	}
+
+}
